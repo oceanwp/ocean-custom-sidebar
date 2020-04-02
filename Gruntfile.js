f6adfa58dd640a6958831ed95e2dfcdb6a09e562
eramits <apprimit@gmail.com>
2020-04-02 08:59:47 +0530

    version updated

---
+++ Gruntfile.js
@@ -0,0 +1,123 @@
+module.exports = function ( grunt ) {
+
+	const sass = require('node-sass');
+
+	// require it at the top and pass in the grunt instance
+	require( 'time-grunt' )( grunt );
+
+	// Load all Grunt tasks
+	require( 'jit-grunt' )( grunt, {
+		makepot: 'grunt-wp-i18n'
+	} );
+
+	grunt.initConfig( {
+
+		pkg: grunt.file.readJSON( 'package.json' ),
+
+		// Newer files checker
+		newer: {
+			options: {
+				override: function ( detail, include ) {
+					if ( detail.task === 'php' || detail.task === 'sass' ) {
+						include( true );
+					} else {
+						include( false );
+					}
+				}
+			}
+		},
+
+		// Watch for changes.
+		watch: {
+			options: {
+				livereload: true,
+				spawn: false
+			},
+		},
+
+		// Copy the theme into the build directory
+		copy: {
+			build: {
+				expand: true,
+				src: [
+					'**',
+					'!node_modules/**',
+					'!build/**',
+					'!.git/**',
+					'!changelog.txt',
+					'!Gruntfile.js',
+					'!package.json',
+					'!package-lock.json',
+					'!.tern-project',
+					'!.gitignore',
+					'!.jshintrc',
+					'!.DS_Store',
+					'!*.map',
+					'!**/*.map',
+					'!**/Gruntfile.js',
+					'!**/package.json',
+					'!**/*~'
+				],
+				dest: 'build/<%= pkg.name %>/'
+			}
+		},
+
+		// Compress build directory into <name>.zip
+		compress: {
+			build: {
+				options: {
+					mode: 'zip',
+					archive: './build/<%= pkg.name %>.zip'
+				},
+				expand: true,
+				cwd: 'build/<%= pkg.name %>/',
+				src: [ '**/*' ],
+				dest: '<%= pkg.name %>/'
+			}
+		},
+
+		makepot: {
+			target: {
+				options: {
+					domainPath: '/languages/', // Where to save the POT file.
+					exclude: [ // Exlude folder.
+						'build/.*',
+						'assets/.*',
+						'readme/.*',
+						'sass/.*',
+						'bower_components/.*',
+						'node_modules/.*'
+					],
+					potFilename: '<%= pkg.name %>.pot', // Name of the POT file.
+					type: 'wp-plugin', // Type of project (wp-plugin or wp-theme).
+					updateTimestamp: true, // Whether the POT-Creation-Date should be updated without other changes.
+					processPot: function ( pot, options ) {
+						pot.headers[ 'plural-forms' ] = 'nplurals=2; plural=n != 1;';
+						pot.headers[ 'last-translator' ] = 'OceanWP\n';
+						pot.headers[ 'language-team' ] = 'OceanWP\n';
+						pot.headers[ 'x-poedit-basepath' ] = '..\n';
+						pot.headers[ 'x-poedit-language' ] = 'English\n';
+						pot.headers[ 'x-poedit-country' ] = 'UNITED STATES\n';
+						pot.headers[ 'x-poedit-sourcecharset' ] = 'utf-8\n';
+						pot.headers[ 'x-poedit-searchpath-0' ] = '.\n';
+						pot.headers[ 'x-poedit-keywordslist' ] = '_esc_attr__;esc_attr_x;esc_attr_e;esc_html__;esc_html_e;esc_html_x;__;_e;__ngettext:1,2;_n:1,2;__ngettext_noop:1,2;_n_noop:1,2;_c;_nc:4c,1,2;_x:1,2c;_ex:1,2c;_nx:4c,1,2;_nx_noop:4c,1,2;\n';
+						pot.headers[ 'x-textdomain-support' ] = 'yes\n';
+						return pot;
+					}
+				}
+			}
+		}
+
+	} );
+
+	// Production task
+	grunt.registerTask( 'build', [
+		'copy'
+	] );
+
+	// Package task
+	grunt.registerTask( 'package', [
+		'compress',
+	] );
+
+};
